ad34ee2184e2f4c15286b64d4bf4abe6b393a56b
Sn1r <71400526+Sn1r@users.noreply.github.com>
2022-02-11 16:27:46 +0200

    Add files via upload

--- handlers.py
+++ handlers.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 from time import sleep
 
@@ -8,7 +7,7 @@ colors = {
 }
 
 def get_files_path():
-    chosen_path = input("Please enter a full path: \n").lower()
+    chosen_path = input("Please enter a full path: \n")
     ext = ['.txt', '.json', '.log', '.conf', '.csv', '.html', '.php', '.py', '.rb', '.xml', '.htm']
     files_full_path = [os.path.join(root, f) for (root, subdirs, files) in os.walk(chosen_path) for f in os.listdir(root) if f.endswith(tuple(ext))]
     return files_full_path
@@ -29,36 +28,4 @@ def find_word_in_files(str_to_find):
                         str_to_find, f"{colors['red']}{str_to_find}{colors['base']}"
                     )
                     new_line = '\n'
-=======
-import os
-from time import sleep
-
-colors = {
-    'red':"\033[91m",
-    'base': "\033[0m"
-}
-
-def get_files_path():
-    chosen_path = input("Please enter a full path: \n").lower()
-    ext = ['.txt', '.json', '.log', '.conf', '.csv', '.html', '.php', '.py', '.rb', '.xml', '.htm']
-    files_full_path = [os.path.join(root, f) for (root, subdirs, files) in os.walk(chosen_path) for f in os.listdir(root) if f.endswith(tuple(ext))]
-    return files_full_path
-
-def find_word_in_files(str_to_find):
-    all_files = get_files_path()
-    # Progress Bar
-    for i in range(21):
-        print ("\r[%-20s] %d%%" % ('='*i, 5*i), end='')
-        sleep(0.01)
-        
-    for file in all_files:
-        with open(file, 'r') as f:
-            lines = f.readlines()
-            for line in lines:
-                if str_to_find in line:
-                    matched_line_prettified = line.replace(
-                        str_to_find, f"{colors['red']}{str_to_find}{colors['base']}"
-                    )
-                    new_line = '\n'
->>>>>>> 2592acf22f7e1cdaff3047e46a1d89d22d2d8083
                     print(f"{new_line}{file}:{matched_line_prettified}")
